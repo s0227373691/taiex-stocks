d18fef1d8e59a373a86a17ff12a18a9c585a8232
Elmer <s0227373691@gms.tku.edu.tw>
2023-12-29 11:42:45 +0800

    fix explore

--- client-side/react/src/pages/Explor/Explor.jsx
+++ client-side/react/src/pages/Explor/Explor.jsx
@@ -1,55 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import fetchSnapshot from '../../services/fetchSnapshot'
-import { useQuery } from 'react-query'
+import React, { useEffect, useState } from "react";
+import fetchSnapshot from "../../services/fetchSnapshot";
+import { useQuery } from "react-query";
 
-import MonthCompute from './MonthCompute/MonthCompute'
-import delay from '../../utils/delay'
-import fetchAllHistorical from '../../services/fetchAllHistorical'
+import MonthCompute from "./MonthCompute/MonthCompute";
+import delay from "../../utils/delay";
+import fetchAllHistorical from "../../services/fetchAllHistorical";
 
 const Explor = () => {
-    const { isLoading, isError, error, isSuccess, data: snapshotDatas } = useQuery('fetchSnapshot', () => fetchSnapshot('TSE,OTC'))
-    const [snapshotFilteredDatas, setSnapshotFilteredDatas] = useState([])
-    const [symbolsData, setSymbolsData] = useState([])
-    const [historicalData, setHistoricalData] = useState([])
+  const {
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+    data: snapshotDatas,
+  } = useQuery("fetchSnapshot", () => fetchSnapshot("TSE,OTC"));
+  const [historicalData, setHistoricalData] = useState([]);
 
-    useEffect(() => {
-        if (snapshotDatas) {
-            let newSymbolsData = []
-            snapshotDatas.map(snapshot => {
-                const newSnapshotData = snapshot.data.filter(el => el.symbol.length === 4)
-                newSymbolsData = [...newSymbolsData, ...newSnapshotData]
-            })
-            setSymbolsData(newSymbolsData)
+  useEffect(() => {
+    if (snapshotDatas) {
+      let newSymbolsData = [];
+      snapshotDatas.map((snapshot) => {
+        const newSnapshotData = snapshot.data.filter(
+          (el) => el.symbol.length === 4
+        );
+        newSymbolsData = [...newSymbolsData, ...newSnapshotData];
+      });
 
-            const getSymbolsAllHistorical = async (symbolsData, i = 0) => {
-                const historical = await fetchAllHistorical(symbolsData[i].symbol, 'M')
-                await delay(1010);
+      const getSymbolsAllHistorical = async (symbolsData, i = 0) => {
+        const historical = await fetchAllHistorical(symbolsData[i].symbol, "M");
+        await delay(1010);
 
-                if (historical === undefined) {
-                    getSymbolsAllHistorical(symbolsData, i)
-                } else {
-                    setHistoricalData(prev => ([...prev, historical]))
-                    getSymbolsAllHistorical(symbolsData, ++i)
-                }
-            }
-            getSymbolsAllHistorical(newSymbolsData)
+        if (historical === undefined) {
+          getSymbolsAllHistorical(symbolsData, i);
+        } else {
+          setHistoricalData((prev) => [...prev, historical]);
+          i++;
+          getSymbolsAllHistorical(symbolsData, i);
         }
-    }, [snapshotDatas])
+      };
+      getSymbolsAllHistorical(newSymbolsData);
+    }
+  }, [snapshotDatas]);
 
-    useEffect(() => console.log(historicalData), [historicalData])
-    return (
-        <div>
-            <h2> Explor </h2>
-            <ul>
-                {historicalData.map(el => {
-                    return (
-                        <MonthCompute key={el.symbol} {...el} />
-                    )
-                })}
-            </ul>
+  useEffect(() => console.log(historicalData), [historicalData]);
+  return (
+    <div>
+      <h2> Explor </h2>
+      <ul>
+        {historicalData.map((el) => {
+          return <MonthCompute key={el.symbol} {...el} />;
+        })}
+      </ul>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Explor
+export default Explor;
