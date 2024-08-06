9fba8ee2aa6e373fc01a483b1bb93f6b45f37ce2
Elmer <s0227373691@gms.tku.edu.tw>
2024-08-06 23:10:14 +0800

    modify: db config env

--- server-side/nodejs/src/config/db.config.js
+++ server-side/nodejs/src/config/db.config.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  console.log('connecting mongoDB ...')
-  await mongoose.connect('mongodb://127.0.0.1:27017/finance');
-  console.log('MongoDB is connected')
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    console.log("connecting mongoDB ...");
+    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wehh2.mongodb.net/${process.env.DB_NAME}`;
+    await mongoose.connect(uri);
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.error("Failed to connect mongoDB");
+  }
 }
