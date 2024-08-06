0ddbd64a480615215b75be190c63cb0a0169958a
Elmer <s0227373691@gms.tku.edu.tw>
2024-08-07 02:48:44 +0800

    modify: create url

--- web/config/finance.ts
+++ web/config/finance.ts
@@ -1,6 +1,6 @@
 const createServerURL = () =>{
     let urlStr = `${process.env.NEXT_PUBLIC_API_SERVER_protocal}//${process.env.NEXT_PUBLIC_API_SERVER_HOST}`
-    process.env.NEXT_PUBLIC_API_SERVER_PORT && (urlStr+=process.env.NEXT_PUBLIC_API_SERVER_PORT)
+    process.env.NEXT_PUBLIC_API_SERVER_PORT && (urlStr+=`:${process.env.NEXT_PUBLIC_API_SERVER_PORT}`)
     return new URL(urlStr)
 }
 
