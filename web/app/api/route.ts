15d7e95f9ba4e6dd021d61ed22a9d381484d41bd
Elmer <s0227373691@gms.tku.edu.tw>
2024-09-20 00:22:27 +0800

    del: api routes

--- web/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-export const revalidate = 60
- 
-export async function GET() {
-  let data = await fetch('https://api.vercel.app/blog')
-  let posts = await data.json()
- 
-  return Response.json(posts)
-}
