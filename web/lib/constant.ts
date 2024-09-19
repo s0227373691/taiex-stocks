40d45ad17eb8f7620a9e3858c783cb7352d596fc
Elmer <s0227373691@gms.tku.edu.tw>
2024-09-19 22:32:01 +0800

    modify: NEXT_PUBLIC_URL string

--- web/lib/constant.ts
+++ web/lib/constant.ts
@@ -1,2 +1,1 @@
-
-export const NEXT_PUBLIC_URL = `${process.env.NEXT_PUBLIC_API_SERVER_protocal}${process.env.NEXT_PUBLIC_API_SERVER_HOST}:${process.env.NEXT_PUBLIC_API_SERVER_PORT}`
+export const NEXT_PUBLIC_URL = `${process.env.NEXT_PUBLIC_API_SERVER_protocal}//${process.env.NEXT_PUBLIC_API_SERVER_HOST}:${process.env.NEXT_PUBLIC_API_SERVER_PORT}`
