383e61b1b1777bdb0b73c0fd0083cf04cca3174f
Elmer <s0227373691@gms.tku.edu.tw>
2024-09-20 00:43:53 +0800

    modify: connect local mongoDB uri

--- server-side/nodejs/src/config/db.config.js
+++ server-side/nodejs/src/config/db.config.js
@@ -4,12 +4,14 @@ main().catch((err) => console.log(err));
 
 async function main() {
   try {
-    console.log(`environment: ${process.env.NODE_ENV}`)
+    console.log(`environment: ${process.env.NODE_ENV}`);
     console.log("connecting mongoDB ...");
 
     let uri;
-    if (process.env.NODE_ENV === "development") uri = `mongodb://localhost:27017/${process.env.DB_NAME}`
-    if (process.env.NODE_ENV === "production") uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wehh2.mongodb.net/${process.env.DB_NAME}`;
+    if (process.env.NODE_ENV === "development")
+      uri = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
+    if (process.env.NODE_ENV === "production")
+      uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wehh2.mongodb.net/${process.env.DB_NAME}`;
     await mongoose.connect(uri);
     console.log("MongoDB is connected");
   } catch (error) {
